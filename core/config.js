0a92d8b1872dcbddc001891d6142af51220381c2
tournamentsakamao-debug <tournamentsakamao@gmail.com>
2025-12-31 10:39:37 +0530

    add core config for global app control

---
+++ core/config.js
@@ -0,0 +1,19 @@
+// core/config.js
+
+window.APP_CONFIG = {
+  appName: "Tournament Sa Kamao",
+
+  // app switches
+  maintenance: false,
+  allowJoin: true,
+  allowCreate: true,
+  allowPayment: true,
+  allowWithdraw: true,
+
+  // admin
+  adminUsername: "admin",
+
+  // audio
+  musicEnabled: true,
+  tapSoundEnabled: true
+};
